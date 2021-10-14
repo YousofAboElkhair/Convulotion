693097848fea478e62fec955c67ec0831ba043c7
YousofAboElkhair <88256260+YousofAboElkhair@users.noreply.github.com>
2021-10-14 15:05:27 +0200

    Create convultion.py

---
+++ convultion.py
@@ -0,0 +1,95 @@
+import learntools.computer_vision.visiontools as visiontools
+from learntools.computer_vision.visiontools import edge, bottom_sobel, emboss, sharpen
+
+kernels = [edge, bottom_sobel, emboss, sharpen]
+names = ["Edge Detect", "Bottom Sobel", "Emboss", "Sharpen"]
+
+plt.figure(figsize=(12, 12))
+for i, (kernel, name) in enumerate(zip(kernels, names)):
+    plt.subplot(1, 4, i+1)
+    visiontools.show_kernel(kernel)
+    plt.title(name)
+plt.tight_layout()
+
+#kernel
+kernel = tf.constant([
+    [-1,-1,-1],
+    [-1,8,-1],
+    [-1,-1,-1],
+])
+
+# Reformat for batch compatibility.
+image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+image = tf.expand_dims(image, axis=0)
+kernel = tf.reshape(kernel, [*kernel.shape, 1, 1])
+kernel = tf.cast(kernel, dtype=tf.float32)
+
+#applying convultion
+conv_fn = tf.nn.conv2d
+
+image_filter = conv_fn(
+    input=image,
+    filters=kernel,
+    strides=1, # or (1, 1)
+    padding='SAME',
+)
+
+plt.imshow(
+    # Reformat for plotting
+    tf.squeeze(image_filter)
+)
+plt.axis('off')
+plt.show();
+
+#applying relu
+relu_fn = tf.nn.relu
+
+mage_detect = relu_fn(image_filter)
+
+plt.imshow(
+    # Reformat for plotting
+    tf.squeeze(image_detect)
+)
+plt.axis('off')
+plt.show();
+
+# Sympy is a python library for symbolic mathematics. It has a nice
+# pretty printer for matrices, which is all we'll use it for.
+import sympy
+sympy.init_printing()
+from IPython.display import display
+
+image = np.array([
+    [0, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [0, 1, 0, 1, 1, 1],
+    [0, 1, 0, 0, 0, 0],
+])
+
+kernel = np.array([
+    [1, -1],
+    [1, -1],
+])
+
+display(sympy.Matrix(image))
+display(sympy.Matrix(kernel))
+# Reformat for Tensorflow
+image = tf.cast(image, dtype=tf.float32)
+image = tf.reshape(image, [1, *image.shape, 1])
+kernel = tf.reshape(kernel, [*kernel.shape, 1, 1])
+kernel = tf.cast(kernel, dtype=tf.float32)
+
+image_filter = tf.nn.conv2d(
+    input=image,
+    filters=kernel,
+    strides=1,
+    padding='VALID',
+)
+image_detect = tf.nn.relu(image_filter)
+
+# The first matrix is the image after convolution, and the second is
+# the image after ReLU.
+display(sympy.Matrix(tf.squeeze(image_filter).numpy()))
+display(sympy.Matrix(tf.squeeze(image_detect).numpy()))
